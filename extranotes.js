e4501582e8335d3a6f3422d149c7a578efc77a1e
shamseena-dev <shamseenakarumarot@gmail.com>
2019-12-15 16:23:01 +0100

    adding files to commit

---
+++ extranotes.js
@@ -0,0 +1,5 @@
+//"build": "webpack --mode development --entry ./js/app2.js --output ./dist/bundle.js",
+
+/*changed to this as per tuts, and it worked
+ */
+//"build": "webpack js/app2.js dist/bundle.js",
